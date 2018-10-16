b8690baa6e4fe53cee8085f58985979da8508b5a
Adam Pisula <adam.pisula@outlook.com>
2018-10-16 17:57:51 +0200

    Better site navigation, store.html template, new Merlin logo (Kherut x Merlin)

--- js/arrow.js
+++ js/arrow.js
@@ -1,17 +1,32 @@
-$(document).ready(function() {
-    $(".arrow_down").on("click", function() {
-        console.log("click");
+function showPage() {
+    var h = 0;
 
+    $(".page").each(function() { h += $(".page").height(); });
+
+    var index = Math.round(-1 * (h - $(window).scrollTop() - h) / $(".page").height());
+
+    for(var i = 0; i < document.getElementsByClassName("page").length; i++) {
+        if(i != index)
+            $(document.getElementsByClassName("page")[i]).addClass("invisible");
+    }
+
+    $(document.getElementsByClassName("page")[index]).removeClass("invisible");
+}
+
+$(document).ready(function() {
+    $(".arrow_up").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $(this).parent().prev().offset().top
+        }, 500);
+    });
+    
+    $(".arrow_down").on("click", function() {
         $('html, body').animate({
             scrollTop: $(this).parent().next().offset().top
         }, 500);
     });
-    
-    $(".arrow_up").on("click", function() {
-        console.log("click");
 
-        $('html, body').animate({
-            scrollTop: $(this).parent().prev().offset().top
-        }, 500);
+    $(window).on("scroll", function() {
+        showPage();
     });
 });
