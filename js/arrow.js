7d44d4134150f58f4eb01a486a983a8af414e97e
Adam Pisula <adam.pisula@outlook.com>
2018-10-29 20:13:52 +0100

    Side menu on home page, new logo, new gradient colors, favicon

--- js/arrow.js
+++ js/arrow.js
@@ -6,14 +6,19 @@ function showPage() {
     var index = Math.round(-1 * (h - $(window).scrollTop() - h) / $(".page").height());
 
     for(var i = 0; i < document.getElementsByClassName("page").length; i++) {
-        if(i != index)
+        if(i != index) {
             $(document.getElementsByClassName("page")[i]).addClass("invisible");
+            $($(".menu li")[i]).removeClass("active");
+        }
     }
 
     $(document.getElementsByClassName("page")[index]).removeClass("invisible");
+    $($(".menu li")[index]).addClass("active");
 }
 
 $(document).ready(function() {
+    showPage();
+
     $(".arrow_up").on("click", function() {
         $('html, body').animate({
             scrollTop: $(this).parent().prev().offset().top
@@ -23,6 +28,12 @@ $(document).ready(function() {
     $(".arrow_down").on("click", function() {
         $('html, body').animate({
             scrollTop: $(this).parent().next().offset().top
+        }, 500);
+    });
+
+    $(".menu li").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#" + $(this).attr("data-id")).offset().top
         }, 500);
     });
 
